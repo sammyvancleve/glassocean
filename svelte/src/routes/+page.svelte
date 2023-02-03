5494999525eb0e5c997e07af90de03ea0a05b442
Sammy Van Cleve <sammyvancleve@gmail.com>
2023-02-02 21:34:48 -0500

    add silly animation

--- svelte/src/routes/+page.svelte
+++ svelte/src/routes/+page.svelte
@@ -2,8 +2,10 @@
 // @ts-nocheck
 
 import { onMount } from "svelte";
+import { fly, slide, fade } from 'svelte/transition';
 
-export let photos = [];
+let photos = [];
+let visible = false;
 
 onMount(async () => {
     const res = await fetch("http://127.0.0.1:8000/images/",
@@ -11,16 +13,39 @@ onMount(async () => {
         method: 'GET',
     });
     photos = await res.json();
+    visible=true;
     await console.log(res);
     await console.log(photos);
 })
 
 </script>
+
 <h1>Glass Ocean</h1>
 <div class="gallery">
 {#each photos as photo}
-    <img src="./{photo.path}" alt={photo.path}/>
-{:else}
-<p>loading...</p>
+{#if visible}
+<figure in:fly="{{y: 15, duration: 750}}">
+    <img src={photo.path} alt={photo.prompt} />
+    <p >{photo.prompt}</p>
+</figure>
+{/if}
 {/each}
 </div>
+
+<style>
+    h1 {
+        font-family: Helvetica, "Trebuchet MS", Verdana, sans-serif;
+    }
+    p {
+        font-family: Helvetica, "Trebuchet MS", Verdana, sans-serif;
+    }
+    .gallery {
+        width: 100%;
+        display: grid;
+        grid-template-columns: repeat(5, 1fr);
+        grid-gap: 8px;
+    }
+    figure, img {
+        width: 100%;
+    }
+</style>
